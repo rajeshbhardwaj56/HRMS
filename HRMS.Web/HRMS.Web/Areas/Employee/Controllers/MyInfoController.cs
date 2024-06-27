7a487bb94670f790194429c0bfa2135f3869e8f6
ram.singh <ram.singh@ELSPC0165>
2024-06-27 22:37:52 +0530

    cgf

--- HRMS.Web/HRMS.Web/Areas/Employee/Controllers/MyInfoController.cs
+++ HRMS.Web/HRMS.Web/Areas/Employee/Controllers/MyInfoController.cs
@@ -86,20 +86,20 @@ namespace HRMS.Web.Areas.Employee.Controllers
             {
                 sendEmailProperties sendEmailProperties = new sendEmailProperties();
                 sendEmailProperties.emailSubject = "Leave Approve Nofification";
-                sendEmailProperties.emailBody = ("Hi "+ rowData.EmployeeFirstName + ", <br/><br/> Your leave have been approved." + "<br/><br/>");
+                sendEmailProperties.emailBody = ("Hi " + rowData.EmployeeFirstName + ", <br/><br/> Your " + rowData.LeaveTypeName + " leave ( from " + rowData.StartDate.ToString("MMMM dd, yyyy") + " to " + rowData.EndDate.ToString("MMMM dd, yyyy") + ") have been approved." + "<br/><br/>");
                 sendEmailProperties.EmailToList.Add(rowData.OfficialEmailID);
                 sendEmailProperties.EmailCCList.Add(rowData.ManagerOfficialEmailID);
                 emailSendResponse response = EmailSender.SendEmail(sendEmailProperties);
-                if (response.responseCode == "200")
-                {
-                    TempData[HRMS.Models.Common.Constants.toastType] = HRMS.Models.Common.Constants.toastTypeSuccess;
-                    TempData[HRMS.Models.Common.Constants.toastMessage] = "Reset password email have been sent, Please reset password for Login.";
-                }
-                else
-                {
-                    TempData[HRMS.Models.Common.Constants.toastType] = HRMS.Models.Common.Constants.toastTypeError;
-                    TempData[HRMS.Models.Common.Constants.toastMessage] = "Reset password email sending failed, Please try again later.";
-                }
+                //if (response.responseCode == "200")
+                //{
+                //    TempData[HRMS.Models.Common.Constants.toastType] = HRMS.Models.Common.Constants.toastTypeSuccess;
+                //    TempData[HRMS.Models.Common.Constants.toastMessage] = "Reset password email have been sent, Please reset password for Login.";
+                //}
+                //else
+                //{
+                //    TempData[HRMS.Models.Common.Constants.toastType] = HRMS.Models.Common.Constants.toastTypeError;
+                //    TempData[HRMS.Models.Common.Constants.toastMessage] = "Reset password email sending failed, Please try again later.";
+                //}
             }
             return Json(new { data = resultsLeaveResultsdata });
         }
